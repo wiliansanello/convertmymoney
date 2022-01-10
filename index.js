6832777bb98e548d08d8cd5be15721847edc6e94
Wilian Ansanello <wiliansanello@outlook.com>
2022-01-10 16:27:14 -0300

    added more coin options to the currency, and url to get currency has changed

--- index.js
+++ index.js
@@ -3,31 +3,36 @@ const app = express()
 const path = require('path')
 
 const convert = require('./lib/convert')
-const apiBCB = require('./lib/api.bcb')
+const apiBCB = require('./lib/api.currency')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async (req, res) => {
-  const cotacao = await apiBCB.getCotacao()
-  console.log('cotacao', cotacao)
-  res.render('home', {
+  /*const cotacao = await apiBCB.getCotacao()
+  const moedaCotacao = moedaCotacao
+  const moedaConversao = moedaConversao
+  if (moedaCotacao && moedaConversao) {
+    console.log(moedaCotacao + ' ' + moedaConversao)
+  }*/
+  //console.log('cotacao', cotacao)
+  res.render('home'/*, {
     cotacao
-  })
+  }*/)
 })
 
 app.get('/cotacao', (req, res) => {
-  res.render('cotacao')
-  console.log(res)
-  const { cotacao, quantidade } = req.query
-  if (cotacao && quantidade) {
-    const conversao = convert.convert(cotacao, quantidade)
+  const { cotacao, moedaCotacao, conversao, moedaConversao } = req.query
+  if (cotacao && conversao) {
+    const resultado = convert.convert(cotacao, conversao)
     res.render('cotacao', {
       error: false,
       cotacao: convert.toMoney(cotacao),
-      quantidade: convert.toMoney(quantidade),
-      conversao: convert.toMoney(conversao)
+      moedaCotacao: moedaCotacao,
+      conversao: convert.toMoney(conversao),
+      moedaConversao: moedaConversao,
+      resultado: convert.toMoney(resultado)
     })
   } else {
     res.render('cotacao', {
